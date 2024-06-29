5281fec9f67f9bb9b5ae0d9e84670c40bdff5f0c
ana-kostadinovska <ana.kostadinovska6@gmail.com>
2024-06-30 01:41:23 +0200

    CRUD for Publisher and Views

--- BookStore.Web/Controllers/PublisherController.cs
+++ BookStore.Web/Controllers/PublisherController.cs
@@ -1,12 +1,122 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BookStore.Domain.Models;
+using BookStore.Service.Interface;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Security.Claims;
+using System.Security.Policy;
+using Publisher = BookStore.Domain.Models.Publisher;
 
 namespace BookStore.Web.Controllers
 {
     public class PublisherController : Controller
     {
+        private readonly IPublisherService _publisherService;
+
+        public PublisherController(IPublisherService publisherService)
+        {
+            _publisherService = publisherService;
+        }
+
         public IActionResult Index()
         {
+            return View(_publisherService.GetAllPublishers());
+        }
+
+        public async Task<IActionResult> Details(Guid? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var publisher = _publisherService.GetDetailsForPublisher(id);
+            if (publisher == null)
+            {
+                return NotFound();
+            }
+
+            return View(publisher);
+        }
+
+        public IActionResult Create()
+        {
             return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create([Bind("Id,Name,City,Email,PhoneNumber")] Publisher publisher)
+        {
+            if (ModelState.IsValid)
+            {
+                publisher.Id = Guid.NewGuid();
+                _publisherService.CreateNewPublisher(publisher);
+                return RedirectToAction(nameof(Index));
+            }
+            return View(publisher);
+        }      
+
+        public IActionResult Edit(Guid? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var publisher = _publisherService.GetDetailsForPublisher(id);
+            if (publisher == null)
+            {
+                return NotFound();
+            }
+            return View(publisher);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(Guid id, [Bind("Id,Name,City,Email,PhoneNumber")] Publisher publisher)
+        {
+            if (id != publisher.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _publisherService.EditExistingPublisher(publisher);
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    throw;
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(publisher);
+        }
+
+        public IActionResult Delete(Guid? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var publisher = _publisherService.GetDetailsForPublisher(id);
+            if (publisher == null)
+            {
+                return NotFound();
+            }
+
+            return View(publisher);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeleteConfirmed(Guid id)
+        {
+            _publisherService.DeletePublisher(id);
+            return RedirectToAction(nameof(Index));
         }
     }
 }
