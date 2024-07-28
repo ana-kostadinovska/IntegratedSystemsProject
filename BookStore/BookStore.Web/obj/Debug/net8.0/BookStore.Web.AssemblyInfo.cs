128bcd385633dc4bf25ca881cb9256573230c3e6
ana-kostadinovska <ana.kostadinovska6@gmail.com>
2024-07-28 17:01:58 +0200

    Added genre and changed header for order details

--- BookStore/BookStore.Web/obj/Debug/net8.0/BookStore.Web.AssemblyInfo.cs
+++ BookStore/BookStore.Web/obj/Debug/net8.0/BookStore.Web.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BookStore.Web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4c04b69b43fef0d0d149f749ae418f2e97163bd6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+06d85dc4db844ba6928fd1fc5a898c412b026b17")]
 [assembly: System.Reflection.AssemblyProductAttribute("BookStore.Web")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BookStore.Web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
