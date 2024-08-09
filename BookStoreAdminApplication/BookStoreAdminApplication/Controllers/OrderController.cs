cc512c6a047857d893f578bbe81e410c06832341
ana-kostadinovska <ana.kostadinovska6@gmail.com>
2024-08-09 22:14:10 +0200

    Export Orders

--- BookStoreAdminApplication/BookStoreAdminApplication/Controllers/OrderController.cs
+++ BookStoreAdminApplication/BookStoreAdminApplication/Controllers/OrderController.cs
@@ -1,4 +1,5 @@
 ﻿using BookStoreAdminApplication.Models;
+using ClosedXML.Excel;
 using GemBox.Document;
 using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
@@ -77,6 +78,48 @@ namespace BookStoreAdminApplication.Controllers
             var stream = new MemoryStream();
             document.Save(stream, new PdfSaveOptions());
             return File(stream.ToArray(), new PdfSaveOptions().ContentType, "ExportInvoice.pdf");
+
+        }
+
+        [HttpGet]
+        public FileContentResult ExportAllOrders()
+        {
+            string fileName = "Orders.xlsx";
+            string contentType = "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet";
+
+            using (var workbook = new XLWorkbook())
+            {
+                IXLWorksheet worksheet = workbook.Worksheets.Add("Orders");
+                worksheet.Cell(1, 1).Value = "OrderID";
+                worksheet.Cell(1, 2).Value = "Customer UserName";
+                worksheet.Cell(1, 3).Value = "Total Price";
+                HttpClient client = new HttpClient();
+                string URL = "http://localhost:5015/api/Admin/GetAllOrders";
+
+                HttpResponseMessage response = client.GetAsync(URL).Result;
+                var data = response.Content.ReadAsAsync<List<Order>>().Result;
+
+                for (int i = 0; i < data.Count(); i++)
+                {
+                    var item = data[i];
+                    worksheet.Cell(i + 2, 1).Value = item.Id.ToString();
+                    worksheet.Cell(i + 2, 2).Value = item.User.UserName;
+                    var total = 0.0;
+                    for (int j = 0; j < item.BooksInOrder.Count(); j++)
+                    {
+                        worksheet.Cell(1, 4 + j).Value = "Book - " + (j + 1);
+                        worksheet.Cell(i + 2, 4 + j).Value = item.BooksInOrder.ElementAt(j).Book.Title;
+                        total += (item.BooksInOrder.ElementAt(j).Quantity * item.BooksInOrder.ElementAt(j).Book.Price);
+                    }
+                    worksheet.Cell(i + 2, 3).Value = total;
+                }
+                using (var stream = new MemoryStream())
+                {
+                    workbook.SaveAs(stream);
+                    var content = stream.ToArray();
+                    return File(content, contentType, fileName);
+                }
+            }
 
         }
 
